0fed010c2f1abb170a298503dabc1153b2ad7388
jagathv <jagathvytheeswaran@Jagaths-MacBook-Pro.local>
2018-03-06 03:16:48 -0800

    'added position/orientation designation config struct'

---
+++ sensors/tools/config.c
@@ -0,0 +1,15 @@
+struct config() {
+
+  //coordinates in 3D space
+  double x_pos;
+  double y_pos;
+  double z_pos;
+
+  //orientation in 3D space
+  double roll;
+  double pitch;
+  double yaw;
+
+  //sensor type string
+  char* sensor_type;
+};
