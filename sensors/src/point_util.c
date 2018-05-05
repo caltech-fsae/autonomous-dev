2052ebe534ab769b2f8e995ce9268c9de87bb5bb
tar-singh <35475658+tar-singh@users.noreply.github.com>
2018-05-05 13:12:42 -0700

    comments

--- sensors/src/point_util.c
+++ sensors/src/point_util.c
@@ -1,5 +1,12 @@
-// using physics coordinates
 #include <math.h>
+#include "point_util.h"
+
+// using physics coordinates
+/*  
+ * Given angles and radius, can convert to a point
+ * in real space. Just changes value of point, doesn't
+ * return anything.
+ */
 spherical_to_cartesian(double theta, double phi, double rad)
 {
     double x;
