2ba661d4eea5e229280eb08843b400aa5aa764a3
eric <ericc3141@users.noreply.github.com>
2018-05-19 12:24:33 -0700

    Working make. I think.

--- mapping/src/map.c
+++ mapping/src/map.c
@@ -5,3 +5,14 @@ int addObj(Mapobj *obj) {
     printf("TODO: Add object %d\n", obj->type);
     return 0;
 }
+
+Object toProto(Mapobj *obj) {
+    Object msg = OBJECT__INIT;
+    msg.x = obj->x;
+    msg.y = obj->y;
+    msg.z = obj->z;
+    msg.type = obj->type;
+    return msg;
+}
+
+Mapobj *toMapobj(Object *obj) {
