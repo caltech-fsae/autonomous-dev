b14a3cb388e393275a72f7ca3e0a7d78b4dc4596
Emma Qian <qianhanmin99@gmail.com>
2018-05-20 02:48:03 -0700

    encoding to lidar transformation

--- sensors/tools/transformation.c
+++ sensors/tools/transformation.c
@@ -6,31 +6,33 @@ double time_to_theta(double time, double freq) {
     return (2 * pi * freq * time);
 }
 
-Matrix matrix_encoding_to_sensor(double distance, double theta,
+Matrix matrix_encoding_to_lidar(double distance, double theta,
         double encoding_lvl) {
     /* Calculate phi based on encoding level. */
     double phi;
+    double height;
     /*
      * TODO: instead of hard coding, have a larger array of smaller 2 value
      * arrays (height and angle) and have the index from large array correspond
      * to encoding level.
     */
     Matrix result;
-    /* Add last column and row of the matrix. */
+    /* Only transformation is from height in encoding. */
     result.mat[0][0] = sin(theta) * cos(phi);
     result.mat[0][1] = cos(theta) * cos(phi);
     result.mat[0][2] = -cos(phi);
-    // result.mat[0][3] =;
+    result.mat[0][3] = 0;
     result.mat[1][0] = sin(theta) * sin(phi);
     result.mat[1][1] = cos(theta) * sin(phi);
     result.mat[1][2] = cos(phi);
-    // result.mat[1][3] =;
+    result.mat[1][3] = 0;
     result.mat[2][0] = cos(theta);
     result.mat[2][1] = -sin(phi);
     result.mat[2][2] = 0;
-    // result.mat[2][3] =;
-    // result.mat[3][0] =;
-    // result.mat[3][1] =;
-    // result.mat[3][2] =;
-    // result.mat[3][3] =;
+    result.mat[2][3] = height;
+    result.mat[3][0] = 0;
+    result.mat[3][1] = 0;
+    result.mat[3][2] = 0;
+    result.mat[3][3] = 1;
+    return result;
 }
