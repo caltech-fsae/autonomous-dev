3c0a02ddbca2ad5c5737e507ed0c814504223aa7
Emma Qian <qianhanmin99@gmail.com>
2018-05-26 13:48:12 -0700

    push

--- sensors/tools/transformation.c
+++ sensors/tools/transformation.c
@@ -1,23 +1,10 @@
-#include <math.h>
 #include "matrix_ops.c"
 
-// for lidar sensor
 double time_to_theta(double time, double freq) {
     /* Time should be in seconds. */
     return (2 * pi * freq * time);
 }
 
-<<<<<<< Updated upstream
-Matrix matrix_encoding_to_lidar(double distance, double theta,
-        double encoding_lvl) {    /* Calculate phi based on encoding level. */
-    double phi;
-    double height;
-    /*
-     * TODO: instead of hard coding, have a larger array of smaller 2 value
-     * arrays (height and angle) and have the index from large array correspond
-     * to encoding level.
-    */
-=======
 // For all functions below:
 // TransformedVector = TranslationMatrix * RotationMatrix * OriginalVector;
 
@@ -27,7 +14,6 @@ Matrix matrix_encoding_to_lidar(double distance, double theta,
 /* Multiply by r, theta, phi. */
 Matrix matrix_spherical_to_cartesian(double theta, double phi) {
     /* Pad with zeroes to turn into a 4x4. */
->>>>>>> Stashed changes
     Matrix result;
     result.mat[0][0] = sin(theta) * cos(phi);
     result.mat[0][1] = cos(theta) * cos(phi);
@@ -48,8 +34,8 @@ Matrix matrix_spherical_to_cartesian(double theta, double phi) {
     return result;
 }
 
-Matrix matrix_encoding_to_lidar(double distance, double theta,
-        double encoding_lvl) {    /* Calculate phi based on encoding level. */
+Matrix matrix_encoding_to_lidar(double theta, double encoding_lvl) {
+    /* Calculate phi based on encoding level. */
     double theta;
     double phi;
     double height;
@@ -60,75 +46,29 @@ Matrix matrix_encoding_to_lidar(double distance, double theta,
     */
     Matrix result;
     /* Only transformation is from height in encoding. */
-    // result = function(theta, phi, 0, 0, 0, height);
+    // result = general_transformation(theta, phi, 0, 0, 0, height);
     return result;
 }
 
 /* Matrix transformation position from lidar to vehicle. */
+// Need translation matrix
 Matrix lidar_to_vehicle() {
     /* TODO: get x, y, z from lidar. */
     double x;
     double y;
     double z;
-    result.mat[0][0] = 1;
-    result.mat[0][1] = 0;
-    result.mat[0][2] = 0;
-    result.mat[0][3] = x;
-    result.mat[1][0] = 0;
-    result.mat[1][1] = 1;
-    result.mat[1][2] = 0;
-    result.mat[1][3] = y;
-    result.mat[2][0] = 0;
-    result.mat[2][1] = 0;
-    result.mat[2][2] = 1;
-    result.mat[2][3] = z;
-    result.mat[3][0] = 0;
-    result.mat[3][1] = 0;
-    result.mat[3][2] = 0;
-    result.mat[3][3] = 1;
+    Matrix result;
+    // result = general_transformation(0, 0, 0, x, y, z);
+    return result;
 }
 
 // Need rotation (yaw, maybe pitch) and translation matrices
 Matrix vehicle_to_world(){
-    // TODO
-}
-
-Point encoding_to_world() {
-    // TODO
-    /*
-    yaw matrix:
-    | cos(x) -sin(x)  0 |
-    | sin(x)  cos(x)  0 |
-    |   0       1     1 |
-
-    pitch matrix:
-    | cos(y)  0  sin(y) |
-    |   0     1    0    |
-    |-sin(y)  0  cos(y) |
-
-    roll matrix:
-    | 1    0       0    |
-    | 0  cos(z) -sin(z) |
-    | 0  sin(z)  cos(z) |
-
-
-    translation matrix:
-    | 1 0 0 X |
-    | 0 1 0 Y |
-    | 0 0 1 Z |
-    | 0 0 0 1 |
-
-    rotation_matrix = yaw * pitch * roll
-    | cos(x)cos(y)  cos(x)sin(y)sin(z) - cos(z)sin(x)  cos(x)cos(z)sin(y) + sin(x)sin(z) |
-    | cos(y)sin(x)  cos(x)cos(z) + sin(x)sin(y)sin(z)  cos(z)sin(x)sin(y) - cos(x)sin(z) |
-    |   -sin(y)           cos(z) + cos(y)sin(z)              cos(y)cos(z) - sin(z)       |
-
-    
-    transformed_point = translation_matrix * rotation_matrix * original_point
-    | cos(x)cos(y)  cos(x)sin(y)sin(z) - cos(z)sin(x)  cos(x)cos(z)sin(y) + sin(x)sin(z)   X |
-    | cos(y)sin(x)  cos(x)cos(z) + sin(x)sin(y)sin(z)  cos(z)sin(x)sin(y) - cos(x)sin(z)   Y |
-    |   -sin(y)           cos(z) + cos(y)sin(z)              cos(y)cos(z) - sin(z)         Z |
-    |      0                        0                                   0                  1 |
-    
-    */
+    double yaw;
+    double pitch;
+    double roll;
+    double x;
+    double y;
+    double z;
+    // result = general_transformation(yaw, pitch, roll, x, y, z);
 }
